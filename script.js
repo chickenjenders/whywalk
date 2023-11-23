f1950608561e34dd01170f998511804efd75ce56
chickenjenders <tylerjenny8@gmail.com>
2023-11-22 21:15:46 -0500

    player

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let player;
 let npc;
-
 function preload() {
-  // Load images for the player and NPC
-  playerImg = loadImage("player.png");
-  npcImg = loadImage("npc.png");
+  playerImg = loadAnimation("./Walk.png", { frameSize: [48, 48], frames: 6 });
+  //img1 = loadAnimation("./idle2.png", { frameSize: [48, 48], frames: 3 });
+  //img2 = loadAnimation("./Walk.png", { frameSize: [32, 32], frames: 6 });
 }
 
 function setup() {
   createCanvas(800, 600);
 
   // Create the player and NPC
-  player = createSprite(width / 2, height / 2, 50, 50);
-  player.addImage(playerImg);
+  player = new Sprite();
+  player.addAni("walk", playerImg, 6);
+  player.diameter = 50;
+  player.rotationLock = true;
+  player.text = "Player";
 
   npc = createSprite(200, 200, 50, 50);
   npc.addImage(npcImg);
