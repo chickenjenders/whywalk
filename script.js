3262104cfa783ce0d81b224625e99bccf777b77b
chickenjenders <tylerjenny8@gmail.com>
2023-11-29 15:04:37 -0500

    maybe

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let cityImg;
 let textArea;
 let bus;
 let sideWalk;
+let score = 0;
+let screen = 0;
+let arcadeFont; // Declare arcadeFont variable
+
 function preload() {
+  // Load assets in the preload function
+  arcadeFont = loadFont("arcadefont.ttf");
   playerImg = loadAnimation("assets/Walk.png", {
     frameSize: [48, 48],
     frames: 6,
@@ -11,42 +17,52 @@ function preload() {
   cityImg = loadImage("assets/city.png", {
     frameSize: [630, 500],
   });
-  //img1 = loadAnimation("./idle2.png", { frameSize: [48, 48], frames: 3 });
-  //img2 = loadAnimation("./Walk.png", { frameSize: [32, 32], frames: 6 });
 }
 
 function setup() {
   createCanvas(630, 600);
-
-  // Create the player and NPC
-  player = new Sprite();
-  player.addAni("walk", playerImg, 6);
-  player.diameter = 50;
-  player.rotationLock = true;
-  player.text = "Player";
-
-  textArea = new Sprite();
-  textArea.w = 630;
-  textArea.h = 100;
-  textArea.y = 530;
-  textArea.color = "gray";
-  textArea.collider = "static";
-  textArea.textSize = 17;
-  textArea.text =
-    "I'm always late to work! How can I make it easier to get there on time?";
-
-  bus = createSprite(258, 231, 96, 17);
-  bus.collider = "static";
-  bus.color = "white";
-
-  sideWalk = createSprite(200, 470, 17, 16);
-  sideWalk.color = "#BAC0D0";
-  sideWalk.collider = "static";
+  //textFont(arcadeFont);
 }
 
 function draw() {
+  if (screen == 0) {
+    menuScreen();
+  } else if (screen == 1) {
+    gameScreen();
+  } else if (screen == 2) {
+    endScreen();
+  }
+}
+
+function gameScreen() {
+  // Create the player and NPC only when the game starts (screen == 1)
+  if (!player) {
+    player = new Sprite();
+    player.addAni("walk", playerImg, 6);
+    player.diameter = 50;
+    player.rotationLock = true;
+    player.text = "Player";
+
+    textArea = new Sprite();
+    textArea.w = 630;
+    textArea.h = 100;
+    textArea.y = 530;
+    textArea.color = "#D0D3D4";
+    textArea.collider = "static";
+    textArea.textSize = 17;
+    textArea.text = "How can cities become more walkable?";
+
+    bus = createSprite(258, 231, 96, 17);
+    bus.collider = "static";
+    bus.color = "white";
+
+    sideWalk = createSprite(200, 470, 17, 16);
+    sideWalk.color = "#BAC0D0";
+    sideWalk.collider = "static";
+  }
+
+  // Draw game elements here
   image(cityImg, 0, 0, 630, 500);
-  player.changeAni("walk");
   if (kb.pressing("left")) {
     player.vel.x = -1.5;
   } else if (kb.pressing("right")) {
@@ -59,4 +75,40 @@ function draw() {
     player.vel.y = 0;
     player.vel.x = 0;
   }
+
+  if (player.colliding(bus)) {
+    textArea.textSize = 14;
+    textArea.text =
+      "Public transportation allows travel for all people in a more eco-friendly and convenient way.";
+  }
+
+  if (player.colliding(sideWalk)) {
+    textArea.textSize = 14;
+    textArea.text =
+      "Well designed and consistent sidewalks help people walk to their destinations safely and efficiently";
+  }
+
+  function drawScore() {
+    fill(0);
+    textAlign(LEFT);
+    textSize(15);
+    text("Score:" + score, 10, 20);
+  }
+}
+
+function menuScreen() {
+  background("blue");
+  textAlign(CENTER);
+  text("CLICK ANYWHERE TO START");
+  stroke("white");
+}
+
+function mousePressed() {
+  if (screen == 0) {
+    startGame();
+  }
+}
+
+function startGame() {
+  screen = 1;
 }
